3475ce42a479942f00cae972b7242f34cfcc788a
Victor Ryabov <victorwillhard@gmail.com>
2024-11-30 15:56:55 +0300

    feat: add CLI commands for producing and consuming Kafka messages

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello, Kafka!")
+import click
+
+from consumer import consume_messages
+from producer import send_message
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command
+@click.option("--kafka", required=True, help="Kafka server (ip:port)")
+@click.option("--topic", required=True, help="Kafka topic")
+@click.option("--message", required=True, help="Message to send")
+def produce(kafka, topic, message):
+    send_message(kafka, topic, message.encode("utf-8"))
+
+
+@cli.command
+@click.option("--kafka", required=True, help="Kafka server (ip:port)")
+@click.option("--topic", required=True, help="Kafka topic")
+def consume(kafka, topic):
+    consume_messages(kafka, topic)
+
+
+if __name__ == "__main__":
+    cli()
