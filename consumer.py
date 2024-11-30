84500217303598a61a5f1889f767d0d9f3f9d29e
Victor Ryabov <victorwillhard@gmail.com>
2024-11-30 17:10:33 +0300

    feat: enhance Kafka consumer configuration with group ID and offset settings

--- consumer.py
+++ consumer.py
@@ -4,7 +4,13 @@ from kafka import KafkaConsumer
 
 @contextmanager
 def _get_consumer(kafka_server, topic):
-    consumer = KafkaConsumer(topic, bootstrap_servers=kafka_server)
+    consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=kafka_server,
+        group_id="default",
+        auto_offset_reset="earliest",
+        enable_auto_commit=True,
+    )
     try:
         yield consumer
     finally:
@@ -14,4 +20,4 @@ def _get_consumer(kafka_server, topic):
 def consume_messages(kafka_server, topic):
     with _get_consumer(kafka_server, topic) as consumer:
         for message in consumer:
-            print(message.value.decode('utf-8'))
+            print(message.value.decode("utf-8"))
